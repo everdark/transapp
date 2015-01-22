9be853a5c4d1097f9bc345f279150380062ddc1b
Kyle Chung <kylechun9@gmail.com>
2015-01-22 16:06:18 +0800

    add server side script

---
+++ torrent-monitor.py
@@ -0,0 +1,53 @@
+
+
+
+import ConfigParser
+import dropbox
+import re
+import subprocess
+import os
+import time
+import daemon
+
+
+def main():
+    while True:
+        # connect to dropbox api
+        access_token = config.get("mydropbox", "access_token")
+        magnet_link_file = config.get("mydropbox", "magnet_file_path")
+        client = dropbox.client.DropboxClient(access_token)
+        # get magnet links and add to transmission server
+        f = client.get_file(magnet_link_file)
+        all_magnets = [line.strip('\n') for line in f if not re.search(r"^#", line)]
+        if len(all_magnets):
+            if subprocess.call(['service', 'transmission-daemon', 'status']):
+                subprocess.call(['service', 'transmission-daemon', 'start'])
+            for m in all_magnets:
+                subprocess.call(['transmission-remote', '-a', m])
+            # replace magnet maintenance file with empty one
+            tmpfname = "tmpfile_for_update_magnet_link"
+            subprocess.call(['touch', tmpfname])
+            f = open(os.path.join(".", tmpfname), 'r')
+            response = client.put_file(magnet_link_file, f, overwrite=True)
+            f.close()
+            os.remove(tmpfname)
+        else:
+            # shutdown transmission daemon if all current torrents are finished downloading
+            incomplete_folder = config.get("transmission_server", "incomplete_path")
+            if not subprocess.call(['service', 'transmission-daemon', 'status']):
+                unfinished = os.listdir(incomplete_folder)
+                if not len(unfinished):
+                    subprocess.call(['service', 'transmission-daemon', 'stop'])
+        time.sleep(60*10)
+
+def runAsDaemon():
+    with daemon.DaemonContext():
+        main()
+
+
+if __name__ == "__main__":
+    config = ConfigParser.ConfigParser()
+    if not len(config.read(['config.ini'])):
+        print "No config file found. Program aborted."
+        exit(1)       
+    runAsDaemon()
