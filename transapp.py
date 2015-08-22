61ef940cc473139bebb1c990a7ae38a26b48616e
kylechung <kylechun9@gmail.com>
2015-08-22 15:12:07 +0800

    correct a bug

--- transapp.py
+++ transapp.py
@@ -1,10 +1,12 @@
 
 # define helper functions for transapp
+import subprocess
 
 def shutdownTransmission():
     """shutdown transmission daemon if all current torrents are finished downloading"""
     cur_torrents = subprocess.check_output(["transmission-remote", "-l"])
-    pcts = [ t.split()[1] for t in cur_torrents[1:-2] ]
+    cur_torrents = [ t for t in cur_torrents.split('\n') if '%' in t ]
+    pcts = [ t.split()[1] for t in cur_torrents ]
     unfinished = [ p for p in pcts if p != "100%" ]
     if not len(unfinished):
         subprocess.call(['service', 'transmission-daemon', 'stop'])
