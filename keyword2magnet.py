ca46d8db69bcf415f054cf3b18b42adac660a324
Kyle Chung <kylechun9@gmail.com>
2015-08-24 18:05:06 +0800

    enhance command line argument interface for keyword2magnet

--- keyword2magnet.py
+++ keyword2magnet.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-import sys
+import argparse
 import urllib2
 from bs4 import BeautifulSoup
 import bencode
@@ -11,21 +11,68 @@ import base64
 import urllib
 
 
+# construct command line argument parser
+def getCommandLineParser():
+    parser = argparse.ArgumentParser(description=
+            "Crawl given bango (over nyaa) and return available magnet link.")
+    parser.add_argument("bango", metavar='bango', type=str, nargs=1, 
+                        help="the bango wanted.")
+    parser.add_argument("-a", "--auto", action="store_true", 
+                        help="automatically return the magnet according to maximum dls")
+    parser.add_argument("-n", "--nitem", metavar='N', type=int, action="store", default=10,
+                        help="maximum number of items listed, default at 10; ignored if -a is used")
+    return parser
+
+def getUserInput(maxn=10):
+    while True:
+        try:
+            chosen = int(raw_input("Enter the wanted number:"))
+        except ValueError:
+            print("Incorrect answer input!")
+            continue
+        else:
+            if 0 <= chosen < maxn: 
+                break
+            else:
+                print("Entering number out of range!")
+                continue
+    return chosen
+
+# define main function
 def main():
-    keyword = sys.argv[1]
-    pagelink = urllib2.urlopen("http://sukebei.nyaa.se/?page=search&cats=0_0&filter=0&term=%s" % keyword)
+    parser = getCommandLineParser()
+    args = parser.parse_args()
+    site = "http://sukebei.nyaa.se/"
+    pagelink = urllib2.urlopen("%s?page=search&cats=0_0&filter=0&term=%s" % (site, args.bango))
     
     # parse nyaa page
-    soup = BeautifulSoup(pagelink, "lxml")
+    soup = BeautifulSoup(pagelink, "lxml") # explicitly require lxml
     tlist = soup.findAll("tr", {"class": "tlistrow"})
+
     if not len(tlist):
         print("No mathcing result.")
         return None
-    dls = [ int(t.find("td", {"class": "tlistdn"}).text) for t in tlist ]
-    chosen = dls.index(max(dls)) # take the one with maximum downloads
-    target = tlist[chosen]
-    name = target.find("td", {"class": "tlistname"}).text.encode("utf-8")
-    link = target.find("td", {"class": "tlistdownload"}).find('a').get("href")
+
+    if args.auto:
+        # take the one with maximum downloads
+        dls = [ int(t.find("td", {"class": "tlistdn"}).text) for t in tlist ]
+        chosen = dls.index(max(dls))
+        target = tlist[chosen]
+        name = target.find("td", {"class": "tlistname"}).text.encode("utf-8")
+        link = target.find("td", {"class": "tlistdownload"}).find('a').get("href")
+    else:
+        # list top 10 and wait for user interact
+        titles = []
+        links = []
+        for t in tlist[:args.nitem]:
+            titles.append(t.find("td", {"class": "tlistname"}).text.encode("utf-8"))
+            links.append(t.find("td", {"class": "tlistdownload"}).find('a').get("href"))
+        for i, t in enumerate(titles):
+            print i, t
+        chosen = getUserInput(maxn=args.nitem)
+        name = titles[chosen]
+        link = links[chosen]
+
     print("File targeted: %s" % name)
 
     # download the chosen torrent
@@ -43,8 +90,4 @@ def main():
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: %s keyword" % sys.argv[0])
-        exit(1)
-    else:
-        main()
+    main()
